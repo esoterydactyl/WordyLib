ddfcb0af34f01829a0566ced57994cecdd90eae7
David Caudill <davidkcaudill@gmail.com>
2013-12-05 21:48:40 -0800

    a basic powershell test.

---
+++ WordyLib/Tests/BasicWordyTests.ps1
@@ -0,0 +1,12 @@
+﻿$path = "C:\Users\david\Documents\GitHub\WordyLib\WordyLib\bin\Debug\WordyLib.dll"
+$HTMLPath = "C:\Users\david\Documents\GitHub\WordyLib\WordyLib\bin\Debug\HtmlAgilityPack.dll"
+Add-Type -Path $path
+Add-Type -Path $HTMLPath
+
+$url = "http://www.dice.com/job/result/10123970/13164?src=19&q=uae"
+
+$webText = [WordyLib.SimpleScraper]::GetWeb($url) 
+$CleanWebText = [WordyLib.SimpleParser]::stripHTML($webText)
+$FilteredWebText = [WordyLib.SimpleCleaner]::degreaseString($CleanWebText)
+
+$SimplifiedScrape = [WordyLib.Wordy]::getKeyWords($url)
