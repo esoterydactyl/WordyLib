9230096b74ca1fc6ac07ec1dc1d4ffae182892d5
David Caudill <davidkcaudill@gmail.com>
2013-12-05 22:11:56 -0800

    Planning a test exe

---
+++ WordyLibTests/WordyLibTests/WordyLibTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordyLib;
+
+
+
+namespace WordyLibTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            Version version = assembly.GetName().Version;
+            Console.WriteLine(version);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
