e8d132befebd4e1a5f1c463b9d288111cc62dd2e
David Caudill <davidkcaudill@gmail.com>
2013-12-07 13:42:20 -0800

    Preparing to capture and track fetch durations.

--- WordyLibTests/WordyLibTests/WordyLibTests/Program.cs
+++ WordyLibTests/WordyLibTests/WordyLibTests/Program.cs
@@ -5,14 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using WordyLib;
 using System.Reflection;
+using System.Diagnostics;
 
 
 namespace WordyLibTests
 {
     class Program
     {
+        public struct testResult
+        {
+            public int durationMS;
+            public string url;
+        }
+
         static void Main(string[] args)
         {
+
+
             System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
             Version version = assembly.GetName().Version;
             string loadMessage = "Loaded Wordy Lib Tests version: " + version.ToString();
@@ -20,14 +29,28 @@ namespace WordyLibTests
             Console.WriteLine("...........");
             Reflector.GetAssemblyInfo();
             Console.WriteLine("Beginning Tests...");
-            string url = "http://www.cnn.com";
-            Console.WriteLine("Reference URL is: " + url);
-            Console.WriteLine("...........");
-
-           var foo = Wordy.getKeywords(url);
-            
-
-
+            string[] urls = { "http://www.cnn.com", "http://www.huffingtonpost.com", "http://www.disney.com", "http://www.wikipedia.org" };
+            List<testResult> resultData = new List<testResult>();
+            foreach (string url in urls)
+            {
+                Console.WriteLine("Reference URL is: " + url);
+                Console.WriteLine("...........");
+                Stopwatch kWFetchStopwatch = new Stopwatch();
+                kWFetchStopwatch.Start();
+                List<string> getKWList = Wordy.getKeywords(url);
+                Console.WriteLine("...........");
+                kWFetchStopwatch.Stop();
+                int kWCount = getKWList.Count;
+                Console.WriteLine("Retrieved " + kWCount.ToString() + " keywords from reference URL.");
+                TimeSpan kwFetchDuration = kWFetchStopwatch.Elapsed;
+                string kwFetchDurationstring = kwFetchDuration.Milliseconds.ToString();
+                Console.WriteLine("This retrieval took: " + kwFetchDurationstring + "ms");
+                Console.WriteLine("...........");
+                testResult urlResult = new testResult();
+                urlResult.durationMS = kwFetchDuration.Milliseconds;
+                urlResult.url = url;
+                resultData.Add(urlResult);
+            }
 
 
             Console.ReadKey();
